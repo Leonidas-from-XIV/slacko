d2976594c94ea8b7ad065a06618b1984e05368e5
Marek Kubica <marek@xivilization.net>
2014-09-17 19:42:12 +0200

    Added command line argument parser

--- src/slack_notify.ml
+++ src/slack_notify.ml
@@ -1,2 +1,18 @@
+let token =
+  let doc = "The Slack API access token" in
+  Cmdliner.Arg.(value & opt string "" & info ["t"; "token"] ~docv:"TOKEN" ~doc)
+
+let info =
+  let doc = "Writes messages to slack" in
+  Cmdliner.Term.info "slack-notify" ~doc
+
+let execute token =
+  "Your token is " ^ token ^ "."
+  |> print_endline
+
+let execute_t = Cmdliner.Term.(pure execute $ token)
+
 let () =
-  print_endline "Too soon"
+  match Cmdliner.Term.eval (execute_t, info) with
+    | `Error _ -> exit 1
+    | _ -> exit 0

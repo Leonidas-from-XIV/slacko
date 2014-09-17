2d77c15960954135aacefd3c556c0ea1be5678be
Marek Kubica <marek@xivilization.net>
2014-09-17 21:35:00 +0200

    Factor out URI generation

--- src/slacko.ml
+++ src/slacko.ml
@@ -1,5 +1,11 @@
+let base_url = "https://slack.com/api/"
+
+let endpoint e =
+  base_url ^ e
+  |> Uri.of_string
+
 let api_test ?foo () =
-  let base = Uri.of_string "https://slack.com/api/api.test" in
+  let base = endpoint "api.test" in
   let uri = match foo with
     | None -> base
     | Some value -> Uri.add_query_param' base ("foo", value) in
@@ -7,7 +13,7 @@ let api_test ?foo () =
   Cohttp_lwt_body.to_string body
 
 let auth_test token =
-  let base = Uri.of_string "https://slack.com/api/auth.test" in
+  let base = endpoint "auth.test" in
   let uri = Uri.add_query_param' base ("token", token) in
   lwt (response, body) = Cohttp_lwt_unix.Client.get uri in
   Cohttp_lwt_body.to_string body

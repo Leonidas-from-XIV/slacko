b123f607a86bc9f8c0f1672754966c7bdc9862ce
Marek Kubica <marek@xivilization.net>
2014-09-26 00:33:37 +0200

    Licensing stuff

--- src/slack_notify.ml
+++ src/slack_notify.ml
@@ -1,3 +1,23 @@
+(*
+* Slacko - Binding to the Slack API
+* Copyright (C) 2014 Marek Kubica <marek@xivilization.net>
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 3.0 of the License, or (at your option) any later version,
+* with the special exception on linking described in file COPYING.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*)
+
 let token =
   let doc = "The Slack API access token" in
   Cmdliner.Arg.(value & opt string "" & info ["t"; "token"] ~docv:"TOKEN" ~doc)

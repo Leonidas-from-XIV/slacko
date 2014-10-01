d7837524c9b8beef717804399afd24bbc91393a1
Marek Kubica <marek@xivilization.net>
2014-10-01 09:45:57 +0200

    Added signatures of remaining functions

--- src/slacko.mli
+++ src/slacko.mli
@@ -66,7 +66,40 @@ val auth_test: string -> apierror Lwt.t
 val channels_history: string -> ?latest:string -> ?oldest:string -> ?count:string -> string -> apierror Lwt.t
 val channels_info: string -> string -> apierror Lwt.t
 val channels_invite: string -> string -> string -> apierror Lwt.t
-
+val channels_join: string -> string -> apierror Lwt.t
+val channels_kick: string -> string -> string -> apierror Lwt.t
+val channels_leave: string -> string -> apierror Lwt.t
+val channels_list: ?exclude_archived:string -> string -> apierror Lwt.t
+val channels_mark: string -> string -> string -> apierror Lwt.t
+val channels_set_purpose: string -> string -> string -> apierror Lwt.t
+val channels_set_topic: string -> string -> string -> apierror Lwt.t
+val chat_delete: string -> string -> string -> apierror Lwt.t
 
 (** Posts message *)
 val chat_post_message: string -> string -> ?username:string -> ?parse:string -> ?icon_url:string -> ?icon_emoji:string -> string -> apierror Lwt.t
+val chat_update: string -> string -> string -> string -> apierror Lwt.t
+val emoji_list: string -> apierror Lwt.t
+val files_info: string -> ?count:string -> ?page:string -> string -> apierror Lwt.t
+val files_list: ?user:string -> ?ts_from:string -> ?ts_to:string -> ?types:string -> ?count:string -> ?page:string -> string -> apierror Lwt.t
+val files_upload: string -> ?filetype:string -> ?filename:string -> ?title:string -> ?initial_comment:string -> ?channels:string -> Cohttp_lwt_body.t -> apierror Lwt.t
+val groups_create: string -> string -> apierror Lwt.t
+val groups_create_child: string -> string -> apierror Lwt.t
+val groups_history: string -> ?latest:string -> ?oldest:string -> ?count:string -> string -> apierror Lwt.t
+val groups_invite: string -> string -> string -> apierror Lwt.t
+val groups_kick: string -> string -> string -> apierror Lwt.t
+val groups_leave: string -> string -> apierror Lwt.t
+val groups_list: ?exclude_archived:string -> string -> apierror Lwt.t
+val groups_mark: string -> string -> string -> apierror Lwt.t
+val groups_set_purpose: string -> string -> string -> apierror Lwt.t
+val groups_set_topic: string -> string -> string -> apierror Lwt.t
+val im_history: string -> ?latest:string -> ?oldest:string -> ?count:string -> string -> apierror Lwt.t
+val im_list: string -> apierror Lwt.t
+val im_mark: string -> string -> string -> apierror Lwt.t
+val oauth_access: string -> string -> ?redirect_url:string -> string -> apierror Lwt.t
+val presence_set: string -> string -> apierror Lwt.t
+val search_all: string -> ?sort:string -> ?sort_dir:string -> ?highlight:string -> ?count:string -> ?page:string -> string -> apierror Lwt.t
+val search_files: string -> ?sort:string -> ?sort_dir:string -> ?highlight:string -> ?count:string -> ?page:string -> string -> apierror Lwt.t
+val search_messages: string -> ?sort:string -> ?sort_dir:string -> ?highlight:string -> ?count:string -> ?page:string -> string -> apierror Lwt.t
+val stars_list: ?user:string -> ?count:string -> ?page:string -> string -> apierror Lwt.t
+val users_list: string -> apierror Lwt.t
+val users_set_active: string -> apierror Lwt.t

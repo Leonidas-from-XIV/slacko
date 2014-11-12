a0f958acf6a1c4deb2f60753ad449c280ebe15a8
Marek Kubica <marek@xivilization.net>
2014-11-12 13:31:15 +0100

    New Travis build script
    This time adapted from CoHTTP.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,129 +1,34 @@
-echo pull req: $TRAVIS_PULL_REQUEST
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  curl https://github.com/$TRAVIS_REPO_SLUG/pull/$TRAVIS_PULL_REQUEST.diff -o pullreq.diff
-else
-  git show > pullreq.diff.tmp
-  merge=`grep "^Merge: " pullreq.diff.tmp | awk -F: '{print $2}'`
-  if [ "$merge" = "" ]; then
-    echo Not a merge
-    mv pullreq.diff.tmp pullreq.diff
-  else
-    echo Merge detected, extracting $merge diff
-    git show $merge > pullreq.diff
-  fi
-fi
+OPAM_DEPENDS="lwt ssl cmdliner cohttp yojson"
 
-full_apt_version () {
-  package=$1
-  version=$2
-  echo -n "${package}="
-  apt-cache show $package | sed -n "s/^Version: \(${version}\)/\1/p" | head -1
-}
-
-install_on_linux () {
-  # Install OCaml and OPAM PPAs
-  case "$OCAML_VERSION,$OPAM_VERSION" in
-  3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
-  3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
-  3.12.1,1.2.0) ppa=avsm/ocaml312+opam12 ;;
-  4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
-  4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
-  4.00.1,1.2.0) ppa=avsm/ocaml40+opam12 ;;
-  4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
-  4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
-  4.01.0,1.2.0) ppa=avsm/ocaml41+opam12 ;;
-  4.02.1,1.1.0) ppa=avsm/ocaml42+opam11 ;;
-  4.02.1,1.2.0) ppa=avsm/ocaml42+opam12 ;;
-  *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
-  esac
-
-  sudo add-apt-repository "deb mirror://mirrors.ubuntu.com/mirrors.txt trusty main restricted universe"
-  sudo add-apt-repository --yes ppa:$ppa
-  sudo apt-get update -qq
-  sudo apt-get install -y $(full_apt_version ocaml-compiler-libs $OCAML_VERSION) \
-                          $(full_apt_version ocaml-interp $OCAML_VERSION) \
-                          $(full_apt_version ocaml-base-nox $OCAML_VERSION) \
-                          $(full_apt_version ocaml-base $OCAML_VERSION) \
-                          $(full_apt_version ocaml $OCAML_VERSION) \
-                          $(full_apt_version ocaml-nox $OCAML_VERSION) \
-                          $(full_apt_version ocaml-native-compilers $OCAML_VERSION) \
-                          $(full_apt_version camlp4 $OCAML_VERSION) \
-                          $(full_apt_version camlp4-extra $OCAML_VERSION) \
-                          opam time
-}
-
-install_on_osx () {
-  curl -OL "http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.6.dmg"
-  sudo hdiutil attach XQuartz-2.7.6.dmg
-  sudo installer -verbose -pkg /Volumes/XQuartz-2.7.6/XQuartz.pkg -target /
-  case "$OCAML_VERSION,$OPAM_VERSION" in
-  4.01.0,1.1.*) brew install opam ;;
-  4.01.0,1.2.*) brew update; brew install opam --HEAD ;;
-  4.02.1,1.1.*) brew install opam ;;
-  4.02.1,1.2.*) brew update; brew install opam --HEAD ;;
-  *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
-  esac
-}
-
-case $TRAVIS_OS_NAME in
-osx) install_on_osx ;;
-linux) install_on_linux ;;
+case "$OCAML_VERSION,$OPAM_VERSION" in
+4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
+4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
+4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
+4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+4.01.0,1.2.0) ppa=avsm/ocaml41+opam12 ;;
+4.02.0,1.1.0) ppa=avsm/ocaml42+opam11 ;;
+4.02.0,1.2.0) ppa=avsm/ocaml42+opam12 ;;
+*) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 
+echo "yes" | sudo add-apt-repository ppa:$ppa
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam time
+
+export OPAMYES=1
 echo OCaml version
 ocaml -version
 echo OPAM versions
 opam --version
 opam --git-version
 
-export OPAMYES=1
+opam init
+opam update
+sudo apt-get install -qq `opam install -e ubuntu ${OPAM_DEPENDS}`
+opam install ${OPAM_DEPENDS}
+eval `opam config env`
 
-cd $TRAVIS_BUILD_DIR
-echo Pull request:
-cat pullreq.diff
-# CR: this will be replaced with the OCamlot analysis of affected packages
-cat pullreq.diff | sed -E -n -e 's,\+\+\+ b/packages/[^/]*/([^/]*)/.*,\1,p' | sort -u > tobuild.txt
-echo To Build:
-cat tobuild.txt
-
-function opam_version_compat {
-  local OPAM_MAJOR OPAM_MINOR ocamlv bytev
-  if [ -n "$opam_version_compat_done" ]; then return; fi
-  opam_version_compat_done=1
-  OPAM_MAJOR=${OPAM_VERSION%%.*}
-  OPAM_MINOR=${OPAM_VERSION#*.}
-  OPAM_MINOR=${OPAM_MINOR%%.*}
-  if [ $OPAM_MAJOR -eq 1 ] && [ $OPAM_MINOR -lt 2 ]; then
-      ocamlv=$(ocamlrun -vnum)
-      bytev=${ocamlv%.*}
-      curl -L https://opam.ocaml.org/repo_compat_1_1.byte$bytev -o compat.byte
-      ocamlrun compat.byte
-  fi
-}
-opam_version_compat
-
-function build_one {
-  pkg=$1
-  echo build one: $pkg
-  rm -rf ~/.opam
-  opam init .
-  case $OCAML_VERSION,$TRAVIS_OS_NAME in
-  4.02.1,osx)
-    opam switch 4.02.1
-    eval `opam config env`
-    ;;
-  esac
-  echo Current switch is:
-  opam switch
-  opam update
-  opam install cmdliner lwt cohttp ocamlfind ssl yojson
-  ocaml setup.ml -configure
-  ocaml setup.ml -build
-  ocaml setup.ml -install
-  ocaml setup.ml -uninstall
-}
-
-echo 'slacko' >> tobuild.txt
-for i in `cat tobuild.txt`; do
-  build_one $i
-done
+ocaml setup.ml -configure
+ocaml setup.ml -build
+ocaml setup.ml -install
+ocaml setup.ml -uninstall

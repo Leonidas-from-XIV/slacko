074d5aacadd54236fed87c77daf03d6d4d952a45
Marek Kubica <marek@xivilization.net>
2014-11-13 08:31:02 +0100

    OPAM 1.1 does not support version constraints

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,4 +1,4 @@
-OPAM_DEPENDS="lwt\>=2.4.6 ssl cmdliner cohttp\>=0.10.0 yojson"
+OPAM_DEPENDS="lwt>=2.4.6 ssl cmdliner cohttp>=0.10.0 yojson"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
 4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
@@ -29,5 +29,5 @@ eval `opam config env`
 oasis setup
 ocaml setup.ml -configure
 ocaml setup.ml -build
-ocaml setup.ml -install
-ocaml setup.ml -uninstall
+sudo ocaml setup.ml -install
+sudo ocaml setup.ml -uninstall

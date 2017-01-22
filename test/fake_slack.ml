b18a02248ffc4fa5fc8927435f4d03e67f105c30
Jeremy Thurgood <firxen@gmail.com>
2017-01-22 13:12:13 +0200

    Handler dispatch in fake_slack.

--- test/fake_slack.ml
+++ test/fake_slack.ml
@@ -8,17 +8,32 @@ open Cohttp_lwt_unix
 let channels_json = Yojson.Safe.from_file "channels.json"
 
 
-let ok_resp key value =
-  `Assoc ["ok", `Bool true; key, value]
+let resp ok fields =
+  let body = `Assoc (("ok", `Bool ok) :: fields) |> Yojson.Safe.to_string in
+  Server.respond_string ~status:`OK ~body ()
+
+let ok_resp fields = resp true fields
+
+let err_resp err fields = resp false (("error", `String err) :: fields)
+
+(* Request handlers *)
+
+let bad_path req body =
+  let path = req |> Request.uri |> Uri.path in
+  err_resp "unknown_method" ["req_method", `String path]
+
+let channels_list req body =
+  ok_resp ["channels", channels_json]
+
+(* Dispatcher, etc. *)
 
 let server ?(port=7357) ~stop () =
   let callback _conn req body =
-    (* let uri = req |> Request.uri |> Uri.to_string in *)
-    (* let meth = req |> Request.meth |> Code.string_of_method in *)
-    (* let headers = req |> Request.headers |> Header.to_string in *)
-    body |> Cohttp_lwt_body.to_string >|= (fun body ->
-    ok_resp "channels" channels_json |> Yojson.Safe.to_string)
-    >>= (fun body -> Server.respond_string ~status:`OK ~body ())
+    let handler = match req |> Request.uri |> Uri.path with
+      | "/api/channels.list" -> channels_list
+      | path -> failwith @@ "Unknown path: " ^ path
+    in
+    handler req body
   in
   Server.create ~mode:(`TCP (`Port port)) ~stop (Server.make ~callback ())
 

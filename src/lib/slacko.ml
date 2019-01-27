f7019a2af530d14d266f240528dcf3ad0550e2cb
Petter A. Urkedal <paurkedal@gmail.com>
2019-01-27 11:38:41 +0100

    Expose functions shared by test through a private module.

--- src/lib/slacko.ml
+++ src/lib/slacko.ml
@@ -18,6 +18,7 @@
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 *)
 
+open Internal
 open Lwt.Infix
 module Cohttp_unix = Cohttp_lwt_unix
 module Cohttp_body = Cohttp_lwt.Body
@@ -208,57 +209,6 @@ type sort_criterion = Score | Timestamp
 type sort_direction = Ascending | Descending
 
 type presence = Auto | Away
-
-let int64_pow b n =
-  let rec loop b n acc =
-    if n = 0 then acc else
-    loop Int64.(mul b b) (n lsr 1)
-         (if n land 1 = 0 then acc else Int64.(mul b acc)) in
-  loop b n 1L
-
-let timestamp_of_string x =
-  let d_ps_of_intlit intlit =
-    let sec = Int64.of_string intlit in
-    let d = Int64.div sec 86_400L in
-    let ps = Int64.(mul (rem sec 86_400L) 1_000_000_000_000L) in
-    (Int64.to_int d, ps) in
-  match
-    match String.split_on_char '.' x with
-    | [_] ->
-        Ptime.Span.of_d_ps (d_ps_of_intlit x)
-    | [sec_lit; subsec_lit] ->
-      let (d, ps_int) = d_ps_of_intlit sec_lit in
-      let ps_frac =
-        if String.length subsec_lit <= 12 then
-          let scale = int64_pow 10L (12 - String.length subsec_lit) in
-          Int64.mul scale (Int64.of_string subsec_lit)
-        else
-          Int64.of_string (String.sub subsec_lit 0 12) in
-      Ptime.Span.of_d_ps (d, Int64.add ps_int ps_frac)
-    | _ -> None
-  with
-  | exception Failure _ -> None
-  | None -> None
-  | Some span -> Ptime.of_span span
-
-let string_of_timestamp ts =
-  let d, ps = Ptime.Span.to_d_ps (Ptime.diff ts Ptime.epoch) in
-  let sec = Int64.(add (mul (of_int d) 86_400L) (div ps 1_000_000_000_000L)) in
-  let subsec = Int64.(rem ps 1_000_000_000_000L) in
-  Printf.sprintf "%Ld.%06Ld" sec (Int64.div subsec 1_000_000L)
-
-let timestamp_of_yojson json =
-  match
-    match json with
-    | `Int x -> Ptime.of_span (Ptime.Span.of_int_s x)
-    | `Intlit x -> timestamp_of_string x
-    | `String x -> timestamp_of_string x
-    | _ -> None
-  with
-  | Some ts -> Result.Ok ts
-  | None -> Result.Error "Couldn't parse timestamp"
-
-let timestamp_to_yojson ts = `String (string_of_timestamp ts)
 
 let user_of_yojson = function
   | `String x -> Result.Ok (UserId x)
--- test/abbrtypes.ml
+++ test/abbrtypes.ml
@@ -3,59 +3,9 @@
    those values at all. Therefore, we copy the record type that use these and
    skip the problematic fields. *)
 
-(* These are copied directly from slacko.ml so we can use them here. *)
+open Slacko__Internal
+
 type timestamp = Ptime.t
-
-let int64_pow b n =
-  let rec loop b n acc =
-    if n = 0 then acc else
-    loop Int64.(mul b b) (n lsr 1)
-         (if n land 1 = 0 then acc else Int64.(mul b acc)) in
-  loop b n 1L
-
-let timestamp_of_string x =
-  let d_ps_of_intlit intlit =
-    let sec = Int64.of_string intlit in
-    let d = Int64.div sec 86_400L in
-    let ps = Int64.(mul (rem sec 86_400L) 1_000_000_000_000L) in
-    (Int64.to_int d, ps) in
-  match
-    match String.split_on_char '.' x with
-    | [_] ->
-        Ptime.Span.of_d_ps (d_ps_of_intlit x)
-    | [sec_lit; subsec_lit] ->
-      let (d, ps_int) = d_ps_of_intlit sec_lit in
-      let ps_frac =
-        if String.length subsec_lit <= 12 then
-          let scale = int64_pow 10L (12 - String.length subsec_lit) in
-          Int64.mul scale (Int64.of_string subsec_lit)
-        else
-          Int64.of_string (String.sub subsec_lit 0 12) in
-      Ptime.Span.of_d_ps (d, Int64.add ps_int ps_frac)
-    | _ -> None
-  with
-  | exception Failure _ -> None
-  | None -> None
-  | Some span -> Ptime.of_span span
-
-let string_of_timestamp ts =
-  let d, ps = Ptime.Span.to_d_ps (Ptime.diff ts Ptime.epoch) in
-  let sec = Int64.(add (mul (of_int d) 86_400L) (div ps 1_000_000_000_000L)) in
-  let subsec = Int64.(rem ps 1_000_000_000_000L) in
-  Printf.sprintf "%Ld.%06Ld" sec (Int64.div subsec 1_000_000L)
-
-let timestamp_of_yojson json =
-  match
-    match json with
-    | `Int x -> Ptime.of_span (Ptime.Span.of_int_s x)
-    | `Intlit x -> timestamp_of_string x
-    | `String x -> timestamp_of_string x
-    | _ -> None
-  with
-  | Some ts -> Result.Ok ts
-  | None -> Result.Error "Couldn't parse timestamp"
-
-let timestamp_to_yojson ts = `String (string_of_timestamp ts)
 
 (* But this one is new. *)
 let pp_timestamp = Ptime.pp_human ~frac_s:6 ()
